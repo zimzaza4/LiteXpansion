7ba11a0207888d6b3f55374697675851e4dfcce3
zimzaza4 <53421735+zimzaza4@users.noreply.github.com>
2020-12-05 12:44:15 +0800

    Update LoreBuilderDynamic.java

--- src/main/java/dev/j3fftw/litexpansion/utils/LoreBuilderDynamic.java
+++ src/main/java/dev/j3fftw/litexpansion/utils/LoreBuilderDynamic.java
@@ -16,7 +16,7 @@ public final class LoreBuilderDynamic {
 
 
     public static String powerBuffer(double power) {
-        return power(power, " Buffer");
+        return power(power, " 已储存");
     }
 
     public static String powerPerTick(double power) {
