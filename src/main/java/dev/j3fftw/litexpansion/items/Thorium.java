7616d128042b241d87f0fb8c879b70d953fa45fc
zimzaza4 <53421735+zimzaza4@users.noreply.github.com>
2020-12-05 09:45:34 +0800

    Update Thorium.java

--- src/main/java/dev/j3fftw/litexpansion/items/Thorium.java
+++ src/main/java/dev/j3fftw/litexpansion/items/Thorium.java
@@ -13,7 +13,7 @@ import org.bukkit.inventory.ItemStack;
 public class Thorium extends UnplaceableBlock implements Radioactive {
 
     private static final ItemStack thorium = new CustomItem(Material.PAPER, "&f提示!",
-        "&a&oMake sure to first GEO-Scan the chunk in which you are", "&a&omining to discover Thorium!");
+        "&a&o确保首先对要开采的区块进行GEO-扫描", "&a&o以开采钍!");
 
     public Thorium() {
         super(Items.LITEXPANSION, Items.THORIUM, RecipeType.GEO_MINER, new ItemStack[] {
